2a8fe6530d2396212aadf2ea159aeec941946df4
Amalia <amaliaroye@gmail.com>
2021-05-10 14:47:26 -0400

    Create data folder

---
+++ src/data/orders.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1, // order id
+    products: [
+      {
+        id: 1, // item id
+        quantity: 1
+      },
+      {
+        id: 2,
+        quantity: 1
+      }
+    ],
+    total: 1.00,
+    isComplete: true,
+    dateCompleted: 123
+  }
+]
---
+++ src/data/users.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    id: 1, // user id
+    email: 'a@a.com',
+    hashedPassword: 'hash',
+    name: 'aa',
+    orders: [
+      {
+        id: 1 // order id
+      }
+    ]
+  }
+]
