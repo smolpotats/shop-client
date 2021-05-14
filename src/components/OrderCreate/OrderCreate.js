8092f95d959f9c22a367be6722842117e30ea910
elainechan55 <emchan55@gmail.com>
2021-05-13 22:47:24 -0500

    Comment out `OrderCreate`

--- src/components/OrderCreate/OrderCreate.js
+++ src/components/OrderCreate/OrderCreate.js
@@ -1,43 +1,41 @@
 /* CREATE - CREATE A NEW ORDER */
 
-import React, { useState } from 'react'
-import StripeCheckout from 'react-stripe-checkout'
+// import React, { useState, Fragment } from 'react'
+// import React, { useState } from 'react'
+
 // import { Link } from 'react-router-dom'
 // import { Link, Redirect } from 'react-router-dom'
-import { orderCreate } from '../../api/orders'
+// import { orderCreate } from '../../api/orders'
+// import OrderForm from '../OrderForm/OrderForm'
 
-const OrderCreate = props => {
-  const [order, setOrder] = useState({ product: null, total: null })
-  const user = props.user
+// const OrderCreate = props => {
+// const [order, setOrder] = useState({ product: null, total: null })
+// const user = props.user
 
-  const handleSubmit = event => {
-    event.preventDefault()
+// const handleChange = event => {
+//   event.persist()
+//
+//   setOrder(prevOrder => {
+//
+//   })
+// }
+//
+// const handleSubmit = event => {
+//   // event.preventDefault()
+//   console.log('event', event)
+//   orderCreate(order, user) // axios call to API
+//     .then(res => setOrder(res.data.order))
+//     .catch(console.error())
+// }
 
-    orderCreate(order, user) // axios call to API
-      .then(res => setOrder(res.data.order))
-      .catch(console.error())
-  }
+// if (order) { // if value is not null
+//   return <Redirect to={'/orders'} />
+// }
 
-  // if (order) { // if value is not null
-  //   return <Redirect to={'/orders'} />
-  // }
+// return (
+//   // make order form component
+//
+// )
+// }
 
-  return (
-    // make order form component
-    // <OrderForm />
-    <StripeCheckout
-      stripeKey='idk_what_this_is_yet'
-      token={handleSubmit}
-      name='Shop Name'
-      description='Enter your information to place order:'
-      // image=''
-      // ComponentClass='div'
-      label='Purchase'
-      // panelLabel='gib monie'
-      // amount={product.price}
-
-    />
-  )
-}
-
-export default OrderCreate
+// export default OrderCreate
